0311668791a0a2b2ac9b1db026c0688d9cc52103
fillipe-felix <felipesoares_1993@hotmail.com>
2021-06-05 17:34:00 -0300

    creating Entities project

---
+++ DevFitness.API/Core/Entities/Meal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DevFitness.API.Core.Entities
+{
+    public class Meal : BaseEntity
+    {
+        public Meal(string description, int calories, DateTime date, int userId) : base()
+        {
+            Description = description;
+            Calories = calories;
+            Date = date;
+            UserId = userId;
+        }
+
+        public string Description { get; private set; }
+        public int Calories { get; private set; }
+        public DateTime Date { get; private set; }
+        public int UserId { get; private set; }
+    }
+}
