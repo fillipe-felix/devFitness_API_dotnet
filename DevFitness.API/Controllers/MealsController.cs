e3e44f62ca4655217bd014d0e66a880799159c74
fillipe-felix <felipesoares_1993@hotmail.com>
2021-06-05 18:24:37 -0300

    creating Controllers

---
+++ DevFitness.API/Controllers/MealsController.cs
@@ -0,0 +1,45 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFitness.API.Controllers
+{
+    // api/users/4/meals
+    [Route("api/users/{userId}/meals")]
+    public class MealsController : ControllerBase
+    {
+        // api/users/4/meals 
+        [HttpGet]
+        public IActionResult GetAll(int userId)
+        {
+            return Ok();
+        }
+
+        // api/users/4/meals/16
+        [HttpGet("{mealId}")]
+        public IActionResult Get(int userId, int mealId)
+        {
+            return Ok();
+        }
+
+        // api/users/4/meals
+        [HttpPost]
+        public IActionResult Post(int userId, [FromBody] CreateMealInputModel inputModel)
+        {
+            return Ok();
+        }
+
+        // api/users/4/meals/16
+        [HttpPut("{mealId}")]
+        public IActionResult Put(int userId, int mealId, [FromBody] UpdateMealInputModel inputModel)
+        {
+            return NoContent();
+        }
+
+        // api/users/4/meals/16
+        [HttpDelete("{mealId}")]
+        public IActionResult Delete(int userId, int mealId)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevFitness.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFitness.API.Controllers
+{
+    // api/users
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        // api/users/1
+        [HttpGet("{id}")]
+        public IActionResult Get( int id)
+        {
+            return Ok();
+        }
+        
+        // api/users
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel inputModel)
+        {
+
+            // isso daqui retorna o id no location do header da requisição
+            return CreatedAtAction(nameof(Get), new {id = 10}, inputModel);
+        }
+        
+        // aí/users/4
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateUserInputModel inputModel)
+        {
+
+            return NoContent();
+        }
+    }
+}
